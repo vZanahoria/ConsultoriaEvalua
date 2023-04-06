3b1a7e49a89eab5e1819aba145692406c65ab7a1
vZanahoria <20030853@itcelaya.edu.mx>
2023-04-06 14:48:47 -0600

    Organizacion pagina de inicio.

--- admin/views/menu.php
+++ admin/views/menu.php
@@ -1,42 +1,9 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Constructora</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Catálogos
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="departamento.php">Departamento</a></li>
-            <li><a class="dropdown-item" href="proyecto.php">Proyecto</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="login.php?action=logout">Cerrar Sesión</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+<div class="container-fluid">
+  <div class="row" id=barra_superior>
+    <div class="col-6" >
+      <img src="images\logoprueba.png"  alt="logo" >
     </div>
+    <div class="col-1">
+      <img src="images\telefono.png">
   </div>
-</nav>
+</div>
