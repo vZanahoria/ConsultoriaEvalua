2746016853e1ae7ef7a982825d834baed4648584
vZanahoria <20030853@itcelaya.edu.mx>
2023-04-10 02:51:47 -0600

    Corrección a los avances.
    Establecimiento del diseño de la pagina de index.

--- admin/views/menu.php
+++ admin/views/menu.php
@@ -1,9 +1,86 @@
 <div class="container-fluid">
   <div class="row" id=barra_superior>
-    <div class="col-6" >
-      <img src="images\logoprueba.png"  alt="logo" >
+    <div class="col-2">&nbsp;</div>
+    <div class="col-8">
+      <img src="images\logoprueba.png" alt="logo">
     </div>
-    <div class="col-1">
-      <img src="images\telefono.png">
+    <div class="col-2">&nbsp;</div>
+  </div>
+
+
+  <div class="row " id=nav_superior>
+    <nav>
+      <div class="row">
+        <div class="col">
+          <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-light menu new_link navbar_color"
+            data-bs-theme="light">
+            <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Carreteras
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="carreteras.html">Nuestros Proyectos</a>
+                      </li>
+                      <li><a class="dropdown-item" href="intercambiador_autopistas.html">Intercambiador de
+                          Autopistas</a>
+                      </li>
+                      <li><a class="dropdown-item" href="viaducto_bicentenario.html">Viaducto Bicentenario</a>
+                      </li>
+                      <li><a class="dropdown-item" href="autovia.html">Autovía Cantábrico</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="ferrocarriles.html">Ferrocarriles</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="puertos.html">Puertos</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Nosotros
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="contacto.html">Contáctanos</a>
+                      </li>
+                      <li><a class="dropdown-item" href="mision.html">Misión</a>
+                      </li>
+                      <li><a class="dropdown-item" href="vision.html">Visión</a>
+                      </li>
+                      <li><a class="dropdown-item" href="valores.html">Valores</a>
+                      </li>
+                      <li><a class="dropdown-item" href="mapa.html">Mapa</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="puertos.html">Login</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <div class="row " id=banner>
+    <div class="col">
+      <div id="imagen_banner">
+        <h1 id="titulo" class="blanco centradotitulo">Grupo Constructor OHLA</h1>
+      </div>
+    </div>
   </div>
 </div>
