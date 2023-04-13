c87b1537cd6fee4ca6bc786d9a2eabaec5a47b88
vZanahoria <20030853@itcelaya.edu.mx>
2023-04-12 23:41:44 -0600

    Create tipo_propiedad.php

---
+++ admin/tipo_propiedad.php
@@ -0,0 +1,63 @@
+<?php
+require_once("controllers/tipo_propiedad.php");
+include_once("views/header_admin.php");
+include_once("views/menu_admin.php");
+
+$action = (isset($_GET['action'])) ? $_GET['action'] : "getAll";
+$id = (isset($_GET['id'])) ? $_GET['id'] : null;
+switch ($action) {
+    case 'new':
+        if (isset($_POST['enviar'])) {
+            $data = $_POST['data'];
+            $cantidad = $tipopropiedad->new($data);
+            if ($cantidad) {
+                $tipopropiedad->flash('success', 'Registro dado de alta con éxito');
+                $data = $tipopropiedad->get(null);
+                include('views/tipo_propiedad/index.php');
+            } else {
+                $tipopropiedad->flash('danger', 'Algo fallo');
+                include('views/tipo_propiedad/form.php');
+            }
+        } else {
+            include('views/tipo_propiedad/form.php');
+        }
+        break;
+    case 'edit':
+        if (isset($_POST['enviar'])) {
+            $data = $_POST['data'];
+            $id = $_POST['data']['id_tipo_propiedad'];
+            $cantidad = $tipopropiedad->edit($id, $data);
+            if ($cantidad) {
+                $tipopropiedad->flash('success', 'Registro actualizado con éxito');
+                $data = $tipopropiedad->get(null);
+                include('views/tipo_propiedad/index.php');
+            } else {
+                $tipopropiedad->flash('danger', 'Algo fallo');
+                $data = $tipopropiedad->get(null);
+                include('views/tipo_propiedad/index.php');
+            }
+        } else {
+            $data = $tipopropiedad->get($id);
+            include('views/tipo_propiedad/form.php');
+        }
+        break;
+    case 'delete':
+        $cantidad = $tipopropiedad->delete($id);
+        if ($cantidad) {
+            $tipopropiedad->flash('success', 'Registro con el id= ' . $id . ' eliminado con éxito');
+            $data = $tipopropiedad->get(null);
+            include('views/tipo_propiedad/index.php');
+        } else {
+            $tipopropiedad->flash('danger', 'Algo fallo');
+            $data = $tipopropiedad->get(null);
+            include('views/tipo_propiedad/index.php');
+        }
+        break;
+    case 'getAll':
+    default:
+        $data = $tipopropiedad->get(null);
+        include("views/tipo_propiedad/index.php");
+        break;
+}
+include("views/footer_admin.php");
+?>
