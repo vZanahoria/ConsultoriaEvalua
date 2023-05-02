8cf3391d7a1bc7bb838b96e83477f3ab6bb7e9e5
vZanahoria <20030853@itcelaya.edu.mx>
2023-05-02 08:38:58 -0600

    Pagina de contacto

---
+++ admin/views/body_contacto.php
@@ -0,0 +1,180 @@
+<div class="row">
+        <div class="col-1">
+            &nbsp;
+        </div>
+        <div class="col-10">
+            <h3 id="texto_experiencia">Pueden contactar con OHLA, a través de los siguientes canales
+            </h3>
+        </div>
+        <div class="col-1">
+            &nbsp;
+        </div>
+    </div>
+    <div class="row">
+        &nbsp;
+    </div>
+
+
+    <div class="row">
+        <div class="col-1">
+            &nbsp;
+        </div>
+        <div class="col-1 barraverde">
+            ______________________
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1">
+            &nbsp;
+        </div>
+        <div class="col-10">
+            <ul>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Dirección: </b>Torre Emperador. Paseo de la Castellana, 259 D-28046 Madrid
+                </li>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Teléfono: </b>(+34) 91 348 41 00
+                </li>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Información general: </b>info@ohla-group.com
+                </li>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Relaciones con los inversores: </b>relacion.accionistas@ohla-group.com
+                </li>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Sostenibilidad: </b>sostenibilidad@ohla-group.com
+                </li>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Empleo: </b>rhseleccion@ohla-group.com
+                </li>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Comunicación e Imagen Corporativa: </b>comunicacion@ohla-group.com
+                </li>
+                <li class="tipografia_txtlarge text-justify">
+                    <b>Protección de datos personales: </b>dpd@ohla-group.com
+                </li>
+            </ul>
+        </div>
+        <div class="col-1">
+            &nbsp;
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            &nbsp;
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-1">
+            &nbsp;
+        </div>
+        <div class="col-1 barraverde">
+            ______________________
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-1">
+            &nbsp;
+        </div>
+        <div class="col-10">
+            <h2 class="titulo_actividad">Consulta tu presupuesto</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-1">
+            &nbsp;
+        </div>
+        <div class="col-5">
+            <form>
+                <div class="row">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Nombre</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                        placeholder="Ingrese su nombre">
+                </div>
+            </form>
+        </div>
+        <div class="col-5">
+            <form>
+                <div class="row">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Correo</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput2"
+                        placeholder="Ingrese su correo electrónico">
+                </div>
+            </form>
+        </div>
+        <div class="col-1">
+            &nbsp;
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            &nbsp;
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-1">
+            &nbsp;
+        </div>
+        <div class="col-5">
+            <form>
+                <div class="row">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Número telefónico</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput3"
+                        placeholder="Ingrese su número telefónico">
+                </div>
+            </form>
+        </div>
+        <div class="col-5">
+            <form>
+                <div class="row">
+
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Asunto</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput4"
+                        placeholder="Ingrese asunto">
+                </div>
+            </form>
+        </div>
+        <div class="col-1">
+            &nbsp;
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            &nbsp;
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            &nbsp;
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Descripción</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+              </div>
+        </div>
+        <div class="col-4">
+            &nbsp;
+        </div>
+    </div>
