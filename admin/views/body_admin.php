0b8f5ea22ed84ccb52311832e9957ee10790fa9b
vZanahoria <20030853@itcelaya.edu.mx>
2023-05-05 15:09:10 -0600

    Formato de diseño

--- admin/views/body_admin.php
+++ admin/views/body_admin.php
@@ -1,5 +1,5 @@
 <h5 class="container text-right p-4 ">
-            <img src="../assets/user.png" width="50px" class="rounded-circle"></h5>
+            <img src="../images/usuario.png" width="50px" class="rounded-circle">Arquitecto</h5>
 
         <div class="container p-4 text-center">
             <div class="p-3 bg-dark text-white">
@@ -11,21 +11,21 @@
                     <div class="row">
                         <div class="col">
                             <button class="btn btn-dark">
-                                <img src="ConsultoriaEvalua/images/usuario.png" width="200px">
+                                <img src="../images/usuario.png" width="200px">
                                 </br>
                                 <h4>Usuarios</h4>
                             </button>
                         </div>
                         <div class="col">
                             <button class="btn btn-dark" onclick="location.href='../clientes/Clientes.php?N=C'">
-                                <img src="../assets/clientes.png" width="200px">
+                                <img src="../images/clientes.png" width="200px">
                                 </br>
                                 <h4>Solicitantes</h4>
                             </button>
                         </div>
                         <div class="col">
                             <button class="btn btn-dark" onclick="location.href='../avaluos/Avaluos.php?N=A'">
-                                <img src="../assets/avaluos.png" width="200px">
+                                <img src="../images/avaluos.png" width="200px">
                                 </br>
                                 <h4>Avaluos</h4>
                             </button>
@@ -33,14 +33,14 @@
 
                         <div class="col">
                             <button class="btn btn-dark" onclick="location.href='../visita/visitas.php?N=W'">
-                                <img src="../assets/visita.png" width="200px">
+                                <img src="../images/visita.png" width="200px">
                                 </br>
                                 <h4>Visitas</h4>
                             </button>
                         </div>
                         <div class="col">
                             <button class="btn btn-dark" onclick="location.href='../expendiente/expendiente.php?N=L'">
-                                <img src="../assets/expediente.png" width="175px">
+                                <img src="../images/expediente.png" width="175px">
                                 </br>
     </br>
                                 <h4>Expedientes</h4>
