0e01ac94a651cad4be49887ada32464e24dc7742
vZanahoria <20030853@itcelaya.edu.mx>
2023-05-28 12:45:53 -0600

    UPDATE diseño cliente, menu_admin

--- admin/views/cliente/index.php
+++ admin/views/cliente/index.php
@@ -1,68 +1,68 @@
-<h1 class="text-center">Gestión de Clientes</h1>
-<div class="row">
-    <div class="col-3">
-    <a href="cliente.php?action=new" class="btn btn-success">Nuevo Cliente</a>
+<div class="container">
+    <h1 class="text-center">Gestión de Clientes</h1>
+    <div class="row">
+        <div class="col-3">
+            <a href="cliente.php?action=new" class="btn btn-success">Nuevo Cliente</a>
+        </div>
+        <div class="col-2">
+            &nbsp;
+        </div>
+        <div class="col-2">
+            <a class="btn btn-success" target="_blank" href="generarExcel.php?modelo=cliente" role="button"><i
+                    class="bi bi-file-earmark-spreadsheet"></i>
+                Generar Excel</a>
+        </div>
     </div>
-    <div class="col-2">
-        &nbsp;
-    </div>
-    <div class="col-2">
-        <a class="btn btn-success" target="_blank" href="generarExcel.php?modelo=cliente" role="button"><i
-                class="bi bi-file-earmark-spreadsheet"></i>
-            Generar Excel</a>
-    </div>
+    <table class="table table-responsive table-bordered table-striped">
+        <thead class="thead-dark">
+            <tr class="table-dark">
+                <th scope="col">#</th>
+                <th scope="col">Apellido Paterno</th>
+                <th scope="col">Apellido Materno</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Teléfono</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Opciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $nReg = 0;
+            foreach ($data as $key => $cliente):
+                $nReg++; ?>
+                <tr>
+                    <td scope="row">
+                        <?php echo $cliente['id_cliente'] ?>
+                    </td>
+                    <td scope="row">
+                        <?php echo $cliente['apellido_paterno'] ?>
+                    </td>
+                    <td scope="row">
+                        <?php echo $cliente['apellido_materno'] ?>
+                    </td>
+                    <td scope="row">
+                        <?php echo $cliente['nombre'] ?>
+                    </td>
+                    <td scope="row">
+                        <?php echo $cliente['telefono'] ?>
+                    </td>
+                    <td scope="row">
+                        <?php echo $cliente['correo'] ?>
+                    </td>
+                    <td scope="row">
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <a href="cliente.php?action=edit&id=<?php echo $cliente['id_cliente'] ?>" type="button"
+                                class="btn btn-primary">Editar</a>
+                            <a href="cliente.php?action=delete&id=<?php echo $cliente['id_cliente'] ?>" type="button"
+                                class="btn btn-danger">Eliminar</a>
+                        </div>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            <tr>
+                <th colspan="7">
+                    Se encontraron <?php echo $nReg ?> registros.
+                </th>
+            </tr>
+        </tbody>
+    </table>
 </div>
-<table class="table table-responsive table-bordered">
-
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Apellido Paterno</th>
-            <th scope="col">Apellido Materno</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Teléfono</th>
-            <th scope="col">Correo</th>
-            <th scope="col">Opciones</th>
-        </tr>
-    <tbody>
-    </tbody>
-    <?php $nReg = 0;
-    foreach ($data as $key => $cliente):
-        $nReg++; ?>
-        <td scope="row">
-            <?php echo $cliente['id_cliente'] ?>
-        </td>
-        <td scope="row">
-            <?php echo $cliente['apellido_paterno'] ?>
-        </td>
-        <td scope="row">
-            <?php echo $cliente['apellido_materno'] ?>
-        </td>
-        <td scope="row">
-            <?php echo $cliente['nombre'] ?>
-        </td>
-        <td scope="row">
-            <?php echo $cliente['telefono'] ?>
-        </td>
-        <td scope="row">
-            <?php echo $cliente['correo'] ?>
-        </td>
-        <th>
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <a href="cliente.php?action=edit&id=<?php echo $cliente['id_cliente'] ?>" type="button"
-                    class="btn btn-primary">Editar</a>
-                <a href="cliente.php?action=delete&id=<?php echo $cliente['id_cliente'] ?>" type="button"
-                    class="btn btn-danger">Eliminar</a>
-            </div>
-
-        </th>
-        </tr>
-    <?php endforeach; ?>
-    <tr>
-        <th>
-            Se encontraron
-            <?php echo $nReg ?> registros.
-        </th>
-    </tr>
-    </thead>
-</table>
